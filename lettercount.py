a7cff0ac5b69aca3dc9a94878bb6b7867bc50d53
Smith <asmith4@MTVL126424808.local>
2014-02-17 14:48:36 -0800

    Check for existence of file before opening it

--- lettercount.py
+++ lettercount.py
@@ -1,38 +1,30 @@
 # import a file
 from sys import argv
+import os
 
 #print argv
 if len(argv) != 2:
-    print "Please enter a filname on the command line"
-
-# Sanitization needed -- check for existence of file
+    print "Please enter the filename only on the command line. "
 
 else:
     filename = argv[1]
     print filename
 
-    twain = open(filename)
+    if not os.path.isfile(filename):
+        print "Sorry, couldn't find file: ", filename
+     
+    else:
+        twain = open(filename)
     
-    ascii_counts = 26*[0]
-    
-    for line in twain:
-        charlist = line.lower()
-        for char in charlist:
-            if char >= 'a' and char <= 'z':
-                ascii_counts[ord(char) - ord('a')] +=1
+        ascii_counts = 26*[0]
+        
+        for line in twain:
+            charlist = line.lower()
+            for char in charlist:
+                if char >= 'a' and char <= 'z':
+                    ascii_counts[ord(char) - ord('a')] +=1
+       
+        twain.close()
 
-    
-    for i in range(len(ascii_counts)):
-      #  print "There are %d instances of the letter %s" % (ascii_counts[i],str(unichr(97+i)))
-      print ascii_counts[i]
-
-
-
-
-
-
- 
-       # for  char in charlist:
-        #    print char
-
-   
+        for i in range(len(ascii_counts)):
+          print ascii_counts[i]
